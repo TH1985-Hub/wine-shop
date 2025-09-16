5c4bc0bdc5837cd5a4140036bb48b3eeca40cabc
Tatevik <htatevik382@gmail.com>
2025-09-16 09:12:15 +0400

    Update all changes

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -1,7 +1,3 @@
-
-
-
-
 import React from 'react';
 import { Typography } from 'antd';
 import { NavLink } from 'react-router-dom';
@@ -12,27 +8,25 @@ import {
 } from '@ant-design/icons';
 import { useLanguage } from '../../contexts/LanguageContext';
 import { footerTexts } from '../../common/texts/footerTexts';
-import logo from './assets/logo.png';
+import logo from './img/logo.png';
 import styles from './Footer.module.css';
 
-const { Title, Text } = Typography;
 
 const Footer = () => {
-  const { currentLanguage } = useLanguage();
-  
- 
+  const { currentLanguage} = useLanguage();
   const texts = footerTexts?.[currentLanguage] || footerTexts.en;
 
+
+  
   return (
-    <div className={styles.footer}>
-      <div className={styles.container}>
-       
-        <div className={styles['logo-wrapper']}>
-          <img 
-            src={logo}
-             alt="Jraghatspanyan Wine" 
-            className={styles.logo}
-          />
+    <footer className={styles.footer}>
+      <div className={styles['footer-content']}>
+
+        {/* Logo Section */}
+        <div className={styles['logo-section']}>
+          <div className={styles['logo-wrapper']}>
+            <img src={logo} alt="Jraghatspanyan Wines" className={styles.logo} />
+          </div>
           <div className={styles['social-icons']}>
             <a href="#" aria-label="Facebook">
               <FacebookFilled />
@@ -45,46 +39,71 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        
-        
-        <div className={styles.columns}>
-          
-          <div className={`${styles.column} ${styles['column--1']}`}>
-            <Title level={4} className={styles['column-header']}>{texts.findUs}</Title>
-            <div className={styles.links}>
-              <a href="#" className={styles.link}>{texts.facebook}</a>
-              <a href="#" className={styles.link}>{texts.instagram}</a>
-              <a href="#" className={styles.link}>{texts.youtube}</a>
-            </div>
+
+      
+        <div className={styles['footer-section']}>
+          <Typography.Title level={5} className={styles['column-header']}>
+            {texts.findUs}
+          </Typography.Title>
+          <div className={styles.links}>
+            <NavLink to="/facebook" className={styles.link}>
+              {texts.facebook}
+            </NavLink>
+            <NavLink to="/instagram" className={styles.link}>
+              {texts.instagram}
+            </NavLink>
+            <NavLink to="/youtube" className={styles.link}>
+              {texts.youtube}
+            </NavLink>
           </div>
-          
-        
-          <div className={`${styles.column} ${styles['column--2']}`}>
-            <Title level={4} className={styles['column-header']}>{texts.information}</Title>
-            <div className={styles.links}>
-              <NavLink to="/about" className={styles.link}>{texts.aboutUs}</NavLink>
-              <NavLink to="/terroir" className={styles.link}>{texts.terroir}</NavLink>
-              <NavLink to="/team" className={styles.link}>{texts.team}</NavLink>
-            </div>
+        </div>
+
+        <div className={styles['footer-section']}>
+          <Typography.Title level={5} className={styles['column-header']}>
+            {texts.information}
+          </Typography.Title>
+          <div className={styles.links}>
+            <NavLink to="/aboutUs" className={styles.link}>
+              {texts.aboutUs}
+            </NavLink>
+
+            <NavLink to="/terror" className={styles.link}>
+              {texts.terroir}
+            </NavLink>
+            <NavLink to="/team" className={styles.link}>
+              {texts.team}
+            </NavLink>
           </div>
-         
-          <div className={`${styles.column} ${styles['column--3']}`}>
-            <Title level={4} className={styles['column-header']}>{texts.feedback}</Title>
-            <div className={styles.links}>
-              <NavLink to="/contact" className={styles.link}>{texts.address}</NavLink>
-              <a href="tel:+1234567890" className={styles.link}>{texts.phoneNumber}</a>
-              <a href="mailto:info@jraghatspanyan.com" className={styles.link}>{texts.emailAddress}</a>
-            </div>
+        </div>
+
+        <div className={styles['footer-section']}>
+          <Typography.Title level={5} className={styles['column-header']}>
+            {texts.feedback}
+          </Typography.Title>
+          <div className={styles.links}>
+            <NavLink to="/address" className={styles.link}>
+              {texts.address}
+            </NavLink>
+            <NavLink to="/Phone Number" className={styles.link}>
+              {texts.phoneNumber}
+            </NavLink>
+            <NavLink to="/email" className={styles.link}>
+              {texts.emailAddress}
+            </NavLink>
           </div>
         </div>
       </div>
-      
+
     
       <div className={styles.copyright}>
-        <Text>© {new Date().getFullYear()} Jraghatspanyan Wines. All rights reserved.</Text>
+        <Typography.Text>
+          {texts.copyright}
+          © 2025 Jraghatspanyan Wines. All rights reserved.
+        </Typography.Text>
       </div>
-    </div>
+    </footer>
   );
 };
 
 export default Footer;
+
