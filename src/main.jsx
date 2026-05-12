b85e3f4a4190cd75916f148b214d0b3ed5a8832d
Accio Assistant <accio-assistant@example.com>
2026-05-12 09:25:03 +0400

    Update all changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,21 @@ import App from "./App.jsx";
 import "@fontsource/jost";
 import "./index.css";
 
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+ 
+//   <BrowserRouter>
+//     <React.StrictMode basename="/wine-shop/">
+//       <App />
+//     </React.StrictMode>
+//   </BrowserRouter>
+
+// );
 const root = createRoot(document.getElementById("root"));
 root.render(
- 
-  <BrowserRouter>
-    <React.StrictMode basename="/wine-shop/">
+  <React.StrictMode>
+    <BrowserRouter basename="/wine-shop/"> {/* Move basename here */}
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
-
+    </BrowserRouter>
+  </React.StrictMode>
 );
